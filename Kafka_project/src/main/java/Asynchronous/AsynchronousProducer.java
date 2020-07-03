05d24ebc30c415527b50b16bde563eb5f64e9889
Reyansh01 <r8.reyansh@gmail.com>
2020-07-03 16:50:29 +0530

    All Files uploading.

---
+++ Kafka_project/src/main/java/Asynchronous/AsynchronousProducer.java
@@ -0,0 +1,38 @@
+package Asynchronous;
+
+import java.util.*;
+import org.apache.kafka.clients.producer.*;
+
+public class AsynchronousProducer {
+
+   public static void main(String[] args) throws Exception{
+      String topicName = "AsynchronousProducerTopic";
+          String key = "Key3";
+          String value = "Value-3";
+
+      Properties props = new Properties();
+      props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+      props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+      Producer<String, String> producer = new KafkaProducer <String, String>(props);
+
+      ProducerRecord<String, String> record = new ProducerRecord<String, String>(topicName,key,value);
+
+      producer.send(record, new MyProducerCallback());
+      System.out.println("AsynchronousProducer call completed");
+      producer.close();
+
+   }
+
+}
+
+    class MyProducerCallback implements Callback{
+
+       public  void onCompletion(RecordMetadata recordMetadata, Exception e) {
+        if (e != null)
+            System.out.println("AsynchronousProducer failed with an exception");
+                else
+                        System.out.println("AsynchronousProducer call Success:");
+       }
+   }
---
+++ Kafka_project/src/main/java/My_package/SimpleConsumer.java
@@ -0,0 +1,51 @@
+package My_package;
+//Based on Fire and Forget.
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class SimpleConsumer {
+	
+	public static void main(String[] args)
+	{
+		Logger logger = LoggerFactory.getLogger(SimpleConsumer.class.getName());
+		
+		String bootstrapServers = "127.0.0.1:9092";
+		String group_Id= "Big_data_ingestion-Kafka";
+		String topic = "SimpleProducerTopic";
+		
+		Properties properties = new Properties();
+		
+		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, group_Id);
+		properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+			
+		//create consumer
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+	
+		//Subscribe consumer to our topics
+		consumer.subscribe(Arrays.asList(topic));
+			
+		//poll for new data
+		while(true)
+		{
+			ConsumerRecords<String, String> records= consumer.poll(Duration.ofMillis(100)); //new in kafka 
+			
+			for(ConsumerRecord<String, String> record: records)
+			{
+				logger.info("Key: " + record.key() + "Value: " + record.value());
+				logger.info("Partition: "+ record.partition() + "Offset: "+ record.offset());
+			}
+		}
+	}
+}
